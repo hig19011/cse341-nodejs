83b5ac0515656da5b95109b375464c61c32491dc
hig19011 <hig19011@byui.edu>
2021-05-03 18:48:43 -0700

    Team Activity 03

--- TeamActivity01/routes/teamRoutes/ta03.js
+++ TeamActivity01/routes/teamRoutes/ta03.js
@@ -1,7 +1,30 @@
-//TA03 PLACEHOLDER
+
 const express = require('express');
 const router = express.Router();
 const https = require('https');
+
+
+router.get('/', (req, res, next) => {
+    getList(data => {
+        res.render('pages/team/ta03', {
+            title: 'Team Activity 03',
+            path: '/ta03',
+            data: data
+        });
+    })
+});
+
+router.post('/', (req, res, next) => {
+
+    getList(data => {
+        data = data.filter(d => d.tags.includes(req.body.keyword));
+        res.render('pages/team/ta03', {
+            title: 'Team Activity 03',
+            path: '/ta03',
+            data: data
+        });
+    });    
+})
 
 const options = {
     hostname: 'byui-cse.github.io',
@@ -10,27 +33,24 @@ const options = {
     method: 'GET'
 }
 
-router.get('/',(req, res, next) => {
+getList = (cb) => {
+    let jsonResponse = '';
+    const dataReq = https.request(options, dataRes => {
 
-    const dataReq = https.request(options, res => {
-        console.log('statusCode: ${req.statusCode')
-
-        res.on('data', d => {
-            process.stdout.write(d);
+        dataRes.on('data', data => {
+            jsonResponse = jsonResponse + data;
         });
-    }, )
+
+        dataRes.on('end', () => {
+            cb(JSON.parse(jsonResponse))
+        });
+    });
 
     dataReq.on('error', error => {
         console.error(error);
     });
 
     dataReq.end();
-
-    //https://byui-cse.github.io/cse341-course/lesson03/items.json
-    res.render('pages/team/ta03', { 
-        title: 'Team Activity 03', 
-        path: '/ta03',
-    });
-});
+}
 
 module.exports = router;
